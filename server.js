0676a6963a75e1c013044eef0d5aebe451ef75db
Yatish <yatish1970@gmail.com>
2022-09-22 19:47:20 -0400

    Changes to be done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var server = express();
 
 var path = require("path")
-
+var router = express.Router();
 function start()
 {
     console.log("Server is started on port")
@@ -12,14 +12,17 @@ server.get("/", function(req,resp)
 {
     resp.send("<h1>Hello how are</h1>")
 })
+var indexPage = require("./apiroutes/index")
 
-server.get("/contact", function(req,resp)
-{
-    resp.send("<h1>We are at Lambtone</h1>")
-})
+server.use('/', router, indexPage)
 server.get("/index", function(req,resp)
 {
     resp.sendFile(path.join(__dirname,"/index.html"))
+})
+
+server.get("/action_page", function(req,resp)
+{
+   // resp.sendFile("Username : " + req.query.)
 })
 
 server.listen(8080, start)
